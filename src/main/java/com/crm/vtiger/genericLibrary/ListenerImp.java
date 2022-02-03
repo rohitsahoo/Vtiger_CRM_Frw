799c2e62b6b7b1a037c76966c31982836a5ee9db
rohitsahoo <rohit.sahoo50@gmail.com>
2022-02-03 09:55:36 +0530

    Commited on 09:55 03/02/2022

--- src/main/java/com/crm/vtiger/genericLibrary/ListenerImp.java
+++ src/main/java/com/crm/vtiger/genericLibrary/ListenerImp.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
-
+import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
@@ -33,6 +33,42 @@ public class ListenerImp implements ITestListener {
 			e.printStackTrace();
 		}
 
+	}
+
+	@Override
+	public void onTestStart(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStart(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onFinish(ITestContext context) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
